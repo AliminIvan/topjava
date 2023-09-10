55debf078d7dd5b65fa78ab8e7c006ce9b3a86e5
Ivan Alimin <aliminivan1990@gmail.com>
2023-09-10 19:24:58 +0300

    HW base task completed:
    - filteredByCycles method implemented
    - getters added to UserMealWithExcess class

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -28,8 +28,42 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with excess. Implement by cycles
-        return null;
+
+        List<UserMealWithExcess> resultList = new ArrayList<>();
+
+        List<UserMealWithExcess> userMealWithExcessList =
+                convertUserMealToUserMealWithExcess(meals, caloriesPerDay);
+
+        for (UserMealWithExcess userMealWithExcess : userMealWithExcessList) {
+            LocalTime mealWithExcessTime = userMealWithExcess.getDateTime().toLocalTime();
+            if (TimeUtil.isBetweenHalfOpen(mealWithExcessTime, startTime, endTime)) {
+                resultList.add(userMealWithExcess);
+            }
+        }
+
+        return resultList;
+    }
+
+    private static List<UserMealWithExcess> convertUserMealToUserMealWithExcess(List<UserMeal> meals, int caloriesPerDay) {
+
+        List<UserMealWithExcess> userMealWithExcessList = new ArrayList<>();
+
+        Map<LocalDate, Integer> map = new HashMap<>();
+
+        for (UserMeal meal : meals) {
+            LocalDate mealDate = meal.getDateTime().toLocalDate();
+            map.merge(mealDate, meal.getCalories(), Integer::sum);
+        }
+
+        for (UserMeal meal : meals) {
+            LocalDate mealDate = meal.getDateTime().toLocalDate();
+            boolean excess = map.get(mealDate) > caloriesPerDay;
+            userMealWithExcessList.add(
+                    new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess));
+        }
+
+        return userMealWithExcessList;
+
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
