795f6537bddada99439f26ae66745b4dbd630802
Ivan Alimin <aliminivan1990@gmail.com>
2023-10-31 14:00:26 +0300

    HW04 move TestRules class from 'repository' to 'topjava'

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.repository.TestRules;
+import ru.javawebinar.topjava.TestRules;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
